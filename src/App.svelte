c6b83be169be578895643f88b7d900da21b4fe4e
OrenBochman <>
2021-11-01 11:02:09 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+  import Editor from './lib/Editor.svelte'
+  import MaterialEditor from './lib/MaterialEditor.svelte'
+</script>
+
+<main>
+  <h1>Hello MarkdDown It!</h1>
+  <MaterialEditor/>
+  <!-- <Editor /> -->  
+  <!-- <Counter /> -->
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 2rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/lib/MaterialEditor.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import Button from '@smui/button';
+  import Card, { Content } from '@smui/card';
+  import Tab, { Label } from '@smui/tab';
+  import TabBar from '@smui/tab-bar';
+	import MarkdownIt from 'markdown-it';
+	import { afterUpdate } from 'svelte';
+
+  /*todo:
+  1. load sample  md from file
+  1. save md to file
+  1. refactor tabs to components (md_edit_box and md_preview)
+  1. persits tab state in a store
+  1. persits md state in a store
+  1. fetch md from ... sanity
+  1. find out how to replace #if :else /if block with a function
+  
+  */ 
+	let active = "edit";
+	const md = new MarkdownIt();
+	const tabs=['edit', 'preview']
+	let markdown:String = '';
+  let output:String = '';
+	//$: output:String = md.render(markdown);	
+	afterUpdate( () => { output = md.render(markdown) } );
+</script>
+
+<svelte:head>
+  <!-- Fonts -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" />
+  <!-- Material Typography -->
+  <link rel="stylesheet" href="https://unpkg.com/@material/typography@11.0.0/dist/mdc.typography.css"
+  />
+  <!-- SMUI -->
+  <link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+</svelte:head>
+
+<section class='editor'>
+<div>
+  <TabBar {tabs} let:tab bind:active>
+    <!-- Note: the `tab` property is required! -->
+    <Tab {tab}>      
+      <Label>{tab}</Label>
+    </Tab>
+  </TabBar>
+</div>
+  {#if active && active === 'edit'}
+  <card>
+    <textarea bind:value={markdown} ></textarea>  
+  </card>
+  {:else}
+  <card ><div class='output'>
+    {@html output}</div>
+    </card>
+  {/if}
+
+<div style="margin-top: 1em;">
+  <div>Programmatically select:</div>
+  {#each tabs as tab}
+  <Button on:click={() => (active = tab)}><Label>{tab}</Label></Button>
+  {/each}
+</div>
+  <pre class="status">Selected: {active}</pre>
+</section>
+
+<style>
+
+.output{
+  text-align:left;
+}
+
+  textarea {
+    box-sizing: inherit;
+		height: 40rem;
+		width: 100%;
+		padding: 1rem;
+		border: 1px solid black;
+  }
+</style>
